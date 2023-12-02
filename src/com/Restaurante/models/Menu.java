989f97374a4687e0e405cb0a87af4248ccf41382
Brayan Diaz <estivel.1995@gmail.com>
2023-12-02 10:35:40 -0500

    Agregacion de clase restaurante metodos constructores y metodos de acceso

--- src/com/Restaurante/models/Menu.java
+++ src/com/Restaurante/models/Menu.java
@@ -4,10 +4,13 @@
  */
 package com.Restaurante.models;
 
+import java.util.Date;
+
 /**
  *
  * @author Usuario
  */
 public class Menu {
-    
+    private String NombreRespresntativo;
+    private Date AnoDeCreacion;
 }
